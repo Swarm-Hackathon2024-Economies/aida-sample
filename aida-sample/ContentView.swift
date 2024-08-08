a4bba10d72c8658326bdf2fc01da4c2912be6110
Yusuke Aida <yusuke_aida@mail.toyota.co.jp>
2024-08-09 08:57:24 +0900

    傾きけんち

--- aida-sample/ContentView.swift
+++ aida-sample/ContentView.swift
@@ -6,17 +6,44 @@
 //
 
 import SwiftUI
+import CoreMotion
+
 
 struct ContentView: View {
+    private var motionManager = CMMotionManager()
+    @State private var x: Double = 0.0
+    @State private var y: Double = 0.0
+    @State private var z: Double = 0.0
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        VStack(spacing: 50) {
+            Text("x: \(x)")
+            Text("y: \(y)")
+            Text("z: \(z)")
         }
-        .padding()
+        .onAppear() {
+            start()
+        }
+        .onDisappear() {
+            stop()
+        }
     }
+    func start() {
+        if motionManager.isAccelerometerAvailable {
+            motionManager.accelerometerUpdateInterval = 0.5
+            motionManager.startAccelerometerUpdates(to: .main) { data, error in
+                guard let data else { return }
+                x = data.acceleration.x
+                y = data.acceleration.y
+                z = data.acceleration.z
+            }
+        }
+    }
+    func stop() {
+        if motionManager.isAccelerometerActive {
+            motionManager.stopAccelerometerUpdates()
+        }
+    }
+    
 }
 
 #Preview {
