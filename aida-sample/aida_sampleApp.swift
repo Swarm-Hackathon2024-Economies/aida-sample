122bdbe32da7e3f860aa30e83c10db209f242b49
Yusuke Aida <yusuke_aida@mail.toyota.co.jp>
2024-08-09 08:16:48 +0900

    Initial Commit

---
+++ aida-sample/aida_sampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  aida_sampleApp.swift
+//  aida-sample
+//
+//  Created by Yusuke Aida on 2024/08/09.
+//
+
+import SwiftUI
+
+@main
+struct aida_sampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
