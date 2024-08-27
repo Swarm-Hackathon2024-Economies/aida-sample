a4c4317ab3a214cd0834024d4229f3aabc9494af
Yusuke Aida <yusuke_aida@mail.toyota.co.jp>
2024-08-27 17:30:50 +0900

    傾き検知のバックグラウンド操作できるか確認

--- aida-sample/ContentView.swift
+++ aida-sample/ContentView.swift
@@ -10,16 +10,39 @@ import CoreMotion
 
 
 struct ContentView: View {
+    
     private var motionManager = CMMotionManager()
     @State private var x: Double = 0.0
-    @State private var y: Double = 0.0
-    @State private var z: Double = 0.0
+    @State private var xCount: Double = 0
+    @State private var y: Double = 0
+    @State private var yCount: Double = 0.0
+    @State private var z: Double = 0
+    @State private var zCount: Double = 0.0
     var body: some View {
         VStack(spacing: 50) {
             Text("x: \(x)")
+                .onChange(of: x) { oldVal, newVal in
+                    if oldVal != newVal {
+                        xCount = xCount + 1
+                    }
+                }
+            Text("x:count \(xCount)")
             Text("y: \(y)")
+                .onChange(of: y) { oldVal, newVal in
+                    if oldVal != newVal {
+                        yCount = yCount + 1
+                    }
+                }
+            Text("yCount: \(yCount)")
             Text("z: \(z)")
+                .onChange(of: z) { oldVal, newVal in
+                    if oldVal != newVal {
+                        zCount = zCount + 1
+                    }
+                }
+            Text("zCount: \(zCount)")
         }
+        
         .onAppear() {
             start()
         }
@@ -27,14 +50,14 @@ struct ContentView: View {
             stop()
         }
     }
-    func start() {
+    func start(_ interval: Double = 0.05) {
         if motionManager.isAccelerometerAvailable {
-            motionManager.accelerometerUpdateInterval = 0.5
+            motionManager.accelerometerUpdateInterval = interval
             motionManager.startAccelerometerUpdates(to: .main) { data, error in
                 guard let data else { return }
-                x = data.acceleration.x
-                y = data.acceleration.y
-                z = data.acceleration.z
+                x = round(data.acceleration.x * 5) / 5
+                y = round(data.acceleration.y * 5) / 5
+                z = round(data.acceleration.z / 2) * 2
             }
         }
     }
